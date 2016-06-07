412bc80c2d41c14999ac1c1b6abc6dfa6744e6fd
DavidJaimeDesign <davidjaimesdesign@gmail.com>
2016-06-06 22:56:09 -0700

    Getting closer with my_each method

--- project2/project2.rb
+++ project2/project2.rb
@@ -2,48 +2,12 @@ module Enumerable
 
 	def my_each(&block)
 		return enum_for(:my_each) unless block_given?
-			if self.class == Range
-				self.to_a
-				n = self.length
-				for n do |i|
-					value = yield i
-					return i 
-				end
-			else 
-				self.to_a
-				n = self.length
-				for n do |i|
-					value = yield i
-					return i
-				end
-			end
-	end
-
-	def my_each_with_index
-	end
-
-	def my_select
-	end
-
-	def my_all
-	end
-
-	def my_any?
-	end
-
-	def my_none
-	end
-
-	def my_count
-	end
-
-	def my_map
-	end
-
-	def my_inject
-	end
-
-	def multiply_els
+		self.to_a
+		for i in 0...self.length
+			yield self[i]
+		end
 	end
 end
 
+[1,2,3,4].my_each{|i| puts i}
+#[1,2,3,4].each{|i| print i*2, " "}
