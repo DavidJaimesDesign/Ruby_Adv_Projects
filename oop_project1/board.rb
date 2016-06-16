28a38a0df085328a46a90e14385230e7c2a056c0
DavidJaimeDesign <davidjaimesdesign@gmail.com>
2016-06-15 22:30:12 -0700

    Added more methods to tic tac toe

--- oop_project1/board.rb
+++ oop_project1/board.rb
@@ -4,47 +4,53 @@ class Board
 		@board = [[nil,nil,nil],[nil,nil,nil],[nil,nil,nil]]
 	end
 
-	def display_board
-		#puts the board with current moves on it 
+	def display_board(board)
+		puts board[0[0]] + " | " + board[0[1]] + " | " + board[0[2]]
+		puts board[1[0]] + " | " + board[1[1]] + " | " + board[1[2]]
+		puts board[2[0]] + " | " + board[2[1]] + " | " + board[2[2]]
+	end
+
+	def move(input,XO)
+		
 	end
 
 	def victory_conditions(board)
 		case victory
 		when board[1] || board[2] || board[3] == ["X","X","X"] 
-			end_of_game(player1)
+			end_of_game("O")
 			return true
 		when board[1] || board[2] || board[3] == ["O","O","O"]
-			end_of_game(player2)
+			end_of_game("O")
 			return true 
 		when board[0[0]] && board[1[0]] && board[2[0]] == "X"
-			end_of_game(player1)
+			end_of_game("X")
 			return true
 		when board[0[1]] && board[1[1]] && board[2[1]] == "X"
-			end_of_game(player1)
+			end_of_game("X")
 			return true
 		when board[0[2]] && board[1[2]] && board[2[2]] == "X"
-			end_of_game(player1)
+			end_of_game("X")
 			return true
 		when board[0[0]] && board[1[0]] && board[2[0]] == "O"
-			end_of_game(player2)
+			end_of_game("O")
 			return true
 		when board[0[1]] && board[1[1]] && board[2[1]] == "O"
-			end_of_game(player2)
+			end_of_game("O")
 			return true
 		when board[0[2]] && board[1[2]] && board[2[2]] == "O"
-			end_of_game(player2)
+			end_of_game("O")
 			return true
 		when board[0[0]] && board[1[1]] && board[2[2]] == "X"
-			end_of_game(player1)
+			end_of_game("X")
 			return true
 		when board[0[0]] && board[1[1]] && board[2[2]] == "O"
-			end_of_game(player2)
+			end_of_game("O")
 			return true
 		when board[0[2]] && board[1[1]] && board[2[0]] == "X"
-			end_of_game(player1)
+			end_of_game("X")
 			return true
 		when board[0[2]] && board[1[1]] && board[2[0]] == "X"
-			end_of_game(player1)
+			end_of_game("X")
 			return true
 		else		 	 
 			return false
@@ -54,9 +60,9 @@ class Board
 	def player(name)
 	end
 
-	def end_of_game(player)
+	def end_of_game(winner)
 		#put end of game condish here?
-		puts player + " WINS BOW DOWN BITCH"
+		puts "#{winner} WINS BOW DOWN BITCH"
 		puts "PLAY AGAIN Y/N?"
 		play_again =  gets.chomp
 		if play_again == "Y"
@@ -64,7 +70,7 @@ class Board
 			@board = [[nil,nil,nil],[nil,nil,nil],[nil,nil,nil]]
 		if play_again == "N"
 			puts "SHUT IT DOWN"
-			#ends the program
+			break
 		else 
 			puts "Please put either Y or N, noob"
 		end
--- oop_project1/tic_tac_toe.rb
+++ oop_project1/tic_tac_toe.rb
@@ -1,7 +1,17 @@
-#0-create a new board
-#1-ask player ones for their names
-#ask player 1 for heads or tails
-#heads player 1 starts tails player 2 starts
-#always start with x
-#board takes input until victory_conditions are met or board is full
-#
+puts "Decide amongst yourselves who will go first X always goes first"
+puts "To make a move simply state where in the graph you want you piece"
+puts "For example top left is A1"
+new_game = Board.new
+
+while victory_conditions(new_game) == false do 
+	puts "X your turn"
+	place_x = gets.chomp
+	#method where they actually add to the board
+	display_board(new_game)
+	puts "O your turn"
+	place_o = gets.chomp
+	#method where they actually add to the board 
+	display_board(new_game)
+end 
+
+
