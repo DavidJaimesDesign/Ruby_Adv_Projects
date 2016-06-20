466b48f034645bf6e20107ba65b0cfae73602047
DavidJaimeDesign <davidjaimesdesign@gmail.com>
2016-06-20 08:54:14 -0700

    Started tdd and method dev

--- oop_project1/Board.rb
+++ oop_project1/Board.rb
@@ -1,94 +1,25 @@
-	def display_board(board)
-		puts "1   2   3  "
-		puts "A " board[0] + " | " + board[1] + " | " + board[2]
-		puts "B " board[1[0]] + " | " + board[1[1]] + " | " + board[1[2]]
-		puts "C " board[2[0]] + " | " + board[2[1]] + " | " + board[2[2]]
-	end
-
-	def move(input,XO)
-		case move
-		when input.capitalize == "A1"
-			board[0[0]] = XO
-		when input.capitalize == "A2"
-			board[0[1]] = XO
-		when input.capitalize == "A3"
-			board[0[2]] = XO
-		when input.capitalize == "B1"
-			board[1[0]] = XO
-		when input.capitalize == "B2"
-			board[1[1]] = XO
-		when input.capitalize == "B3"
-			board[1[2]] = XO
-		when input.capitalize == "C1"
-			board[2[0]] = XO
-		when input.capitalize == "C2"
-			board[2[1]] = XO
-		when input.capitalize == "C3"
-			board[2[2]] = XO
-		else 
-			puts "Wrong input please try again"
-	end
-
-	def victory_conditions(board)
-		case victory
-		when board[1] || board[2] || board[3] == ["X","X","X"] 
-			end_of_game("O")
-			return true
-		when board[1] || board[2] || board[3] == ["O","O","O"]
-			end_of_game("O")
-			return true 
-		when board[0[0]] && board[1[0]] && board[2[0]] == "X"
-			end_of_game("X")
-			return true
-		when board[0[1]] && board[1[1]] && board[2[1]] == "X"
-			end_of_game("X")
-			return true
-		when board[0[2]] && board[1[2]] && board[2[2]] == "X"
-			end_of_game("X")
-			return true
-		when board[0[0]] && board[1[0]] && board[2[0]] == "O"
-			end_of_game("O")
-			return true
-		when board[0[1]] && board[1[1]] && board[2[1]] == "O"
-			end_of_game("O")
-			return true
-		when board[0[2]] && board[1[2]] && board[2[2]] == "O"
-			end_of_game("O")
-			return true
-		when board[0[0]] && board[1[1]] && board[2[2]] == "X"
-			end_of_game("X")
-			return true
-		when board[0[0]] && board[1[1]] && board[2[2]] == "O"
-			end_of_game("O")
-			return true
-		when board[0[2]] && board[1[1]] && board[2[0]] == "X"
-			end_of_game("X")
-			return true
-		when board[0[2]] && board[1[1]] && board[2[0]] == "X"
-			end_of_game("X")
-			return true
-		else		 	 
-			return false
-	end
-
-	def player(name)
-	end
-
-	def end_of_game(winner)
-		#put end of game condish here?
-		puts "#{winner} WINS BOW DOWN BITCH"
-		puts "PLAY AGAIN Y/N?"
-		play_again =  gets.chomp
-		if play_again == "Y"
-			#Reset the board and start again
-			@board = [[nil,nil,nil],[nil,nil,nil],[nil,nil,nil]]
-		if play_again == "N"
-			puts "SHUT IT DOWN"
-			break
-		else 
-			puts "Please put either Y or N, noob"
-		end
-	end
-=end
+def move(arr, square, value)
+	arr[square] = value
 end
 
+def victory_conditions(arr, value)
+	if arr[0..2] == value
+		true
+	elsif arr[3..5] == value
+		true
+	elsif arr[6..8] == value
+		true
+	elsif arr[0] == value && arr[3] == value && arr[6] == value
+		true
+	elsif arr[1] == value && arr[4] == value && arr[5] == value
+		true
+	elsif arr[2] == value && arr[5] == value && arr[8] == value
+		true
+	elsif arr[0] == value && arr[4] == value && arr[8] == value
+		true
+	elsif arr[6] == value && arr[4] == value && arr[2] == vlaue
+		true
+	else
+		 false
+	end
+end
--- oop_project1/board_spec.rb
+++ oop_project1/board_spec.rb
@@ -1,56 +1,30 @@
 require "Board"
 
-describe Board do
-  context "creating" do
-    it "should create an empty 3 by 3 2D array" do
-      expect(test_board = Board.new).to eq([[nil,nil,nil],[nil,nil,nil],[nil,nil,nil]])
-    end
-  end
-end
-=begin
-describe "#display_board" do
-  it "Displays the board with the current filed spots" do
-    expect(something).to eq()
-  end
-end
-describe "#move" do
-  it "does something" do
-    expect(something).to eq()
-  end
-end
-describe "#victory_conditions" do
-  it "does something" do
-    expect(something).to eq()
-  end
-end
-describe "#player" do
-  it "does something" do
-    expect(something).to eq()
-  end
-end
-describe "#end_of_game" do
-  it "does something" do
-    expect(something).to eq()
+describe "The move board" do 
+  it "Places either x or 0 on the board given the posisition" do
+    board = Array.new(9, nil)
+    expect(move(board, 1, "X")).to eq(board[1] = "X")
   end
 end
 
-
-# http://en.wikipedia.org/wiki/Factorial
-describe "#factorial" do
-  it "computes the factorial of 0" do
-    expect(factorial(0)).to eq(1)
-  end
-  it "computes the factorial of 1" do
-    expect(factorial(1)).to eq(1)
-  end
-  it "computes the factorial of 2" do
-    expect(factorial(2)).to eq(2)
-  end
-  it "computes the factorial of 5" do
-    expect(factorial(5)).to eq(120)
-  end
-  it "computes the factorial of 10" do
-    expect(factorial(10)).to eq(3628800)
+describe "the victory conditions method" do 
+  it "returns false uness victory conditions have been met" do
+    board = Array.new(9, nil)
+    expect(victory_conditions(board, "X")).to eq(false)
+    board[0] = "X"
+    board[1] = "X"
+    board[2] = "X"
+    expect(victory_conditions(board, "X")).to eq(true)
+    board[0..8] = nil
+    board[0] = "X"
+    board[1] = "O"
+    board[2] = "X"
+    board[3] = "O"
+    board[4] = "O"
+    board[5] = "X"
+    board[6] = "O"
+    board[7] = "X"
+    board[8] = "O"
+    expect(victoy_conditions(board, "X")).to eq(false)
   end
 end
-=end
