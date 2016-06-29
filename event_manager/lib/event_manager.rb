d0d69b6b37839360551d6d1f27301cb0638e948b
DavidJaimeDesign <davidjaimesdesign@gmail.com>
2016-06-28 22:06:54 -0700

    Started serialization and I/O project

---
+++ event_manager/lib/event_manager.rb
@@ -0,0 +1,33 @@
+require 'csv'
+require 'sunlight/congress'
+
+template_letter = File.read "form_letter.html"
+
+Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
+
+def clean_zipcode(zipcode)
+	zipcode.to_s.rjust(5, "0")[0..4]
+end
+
+def legislators_by_zipcode(zipcode)
+		legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+
+	legislators_names = legislators.collect do |legislator|
+		"#{legislator.first_name} #{legislator.last_name}"
+	end
+	legislators_names.join(", ")
+end
+
+puts "EventManager initialized!"
+
+contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+contents.each do |row|
+	name = row[:first_name]
+	zipcode = clean_zipcode(row[:zipcode])
+	legislators = legislators_by_zipcode(zipcode)
+
+	personal_letter = template_letter.gsub('FIRST_NAME',name)
+	personal_letter.gsub('LEGISLATORS',legislators)
+
+	puts "#{name} #{zipcode} #{legislators}"
+end
