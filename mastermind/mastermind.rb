bcf59015b9ca87885fa4059b2d1cc0b5670437b3
David Jaimes <davidjaimesdesign@gmail.com>
2016-09-23 05:43:48 -0700

    Refactored mastermind.rb for Sinatra

--- mastermind/mastermind.rb
+++ mastermind/mastermind.rb
@@ -1,54 +1,51 @@
-colors = ["black", "white", "red", "blue", "yellow", "green"]
-blank_board = ["", "", "", ""]
+class Mastermind
+	attr_accessor :board, :guess_count, :white_pin_arr, :red_pin_arr, :guess
 
-def code_gen(board)
-	colors = ["black", "white", "red", "blue", "yellow", "green"]
-	board[0] = colors[rand(0..5)]
-	board[1] = colors[rand(0..5)]
-	board[2] = colors[rand(0..5)]
-	board[3] = colors[rand(0..5)]
-end
-
-def guess_check(board,guess)
-	@board = board
-	@guess = guess
-	red_pin_arr = Array.new
-	no_pin_arr = guess - board
-
-	board.each_with_index do |value, index|
-		if board[index] == guess[index]
-			red_pin_arr.push(value)
-		end
+	def initialize
+		@board           = [1,2,3,4]
+		@white_pin_arr   = Array.new
+		@red_pin_arr 	 = Array.new
 	end
 
-	white_pin_arr = guess - no_pin_arr - red_pin_arr
-	
-	puts "There are #{white_pin_arr.length} white pins"
-	puts "There are #{red_pin_arr.length} red pins"
+	def guess_check(guess)
+		@guess = guess
+		guess = guess.split('')
+		guess_arr = guess.map!{|v| v.to_i}
+		
+		puts guess_arr.inspect	
+		no_pin_arr = guess - board
 
+		board.each_with_index do |value, index|
+			if board[index] == guess[index]
+				@red_pin_arr.push(value)
+			end
+		end
+
+		@white_pin_arr = guess - no_pin_arr - @red_pin_arr
+	end
+
+	def display_white_pins
+		return "You have #{@white_pin_arr.length} correct digits but not in in the correct order"
+	end
+
+	def display_red_pins
+		return "You have #{@red_pin_arr.length} correct digits in the correct order"	
+	end
 end
 
 count = 0
-board = Array.new(4)
-code_gen(board)
-test_rig = ["blue", "blue", "green", "black"]
+#board = Array.new(4)
+#code_gen(board)
+#test_rig = ["blue", "blue", "green", "black"]
+game = Mastermind.new
 
 while count < 12 do
-	puts "Color for the 1st spot"
-	blank_board[0] = gets.chomp
-	puts "Color for the 2nd spot"
-	blank_board[1] = gets.chomp
-	puts "Color for the 3rd spot"
-	blank_board[2] = gets.chomp
-	puts "Color for the 4th spot"
-	blank_board[3] = gets.chomp
-	puts "C1 = #{blank_board[0]} C2 = #{blank_board[1]} C3 = #{blank_board[2]} C4 = #{blank_board[3]}"
+	puts "Number sequence pls:"
+	guess = gets.chomp
 
-	guess_check(board, blank_board)
-	if board == blank_board 
-		"Game Over You have defeated the best neural net in the world"
-		break
-	end
+	game.guess_check(guess)
+	puts game.display_red_pins
+	puts game.display_white_pins
 
 	count += 1
 end
